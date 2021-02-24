2abf3ecd6bb2b74ab5bb745f218380ad56a52b6e
acsalves0 <acsalourencioalves@gmail.com>
2021-02-24 10:03:48 -0300

    focopraticagrupo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { Profile } from '../components/Profile'
 import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 
 export default function Home() {
@@ -26,7 +27,9 @@ export default function Home() {
          <CompletedChallenges/>
          <Countdown/>
        </div>
-       <div></div>
+       <div>
+         <ChallengeBox/>
+       </div>
      </section>
     </div>
   )
